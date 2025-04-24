2a791eeea1c91cc35cb6afc3ed42379af66d5431
d-a-p <11903184+d-a-p@users.noreply.github.com>
2025-04-24 18:23:13 +0530

    adding tnc component

--- src/components/MaximiseRewards.jsx
+++ src/components/MaximiseRewards.jsx
@@ -8,34 +8,45 @@ import { useNavigate } from 'react-router-dom';
 
 const creditCards = [
   {
+    name: "Bank of America Customized Cash Rewards Credit Card",
+    number: "1234 5678 9101 XXXX",
+    owner: "Mona Lisa",
+    card_type: "Reward Card"
+  },
+  {
+    name: "Capital One Platinum Credit Card",
+    number: "1234 5678 9101 XXXX",
+    owner: "Mona Lisa",
+    card_type: "Reward Card"
+
+  },
+  {
+    name: "Chase Sapphire Preferred",
+    number: "1234 5678 9101 XXXX",
+    owner: "Mona Lisa",
+    card_type: "Reward Card"
+
+  },
+  {
     name: "Spiderman CARD",
     number: "1234 5678 9101 XXXX",
     owner: "Mona Lisa",
+    card_type: "Reward Card"
+
   },
   {
     name: "Black Panther CARD",
     number: "1234 5678 9101 XXXX",
     owner: "Mona Lisa",
+    card_type: "Reward Card"
+
   },
   {
     name: "Thor CARD",
     number: "1234 5678 9101 XXXX",
     owner: "Mona Lisa",
-  },
-  {
-    name: "Spiderman CARD",
-    number: "1234 5678 9101 XXXX",
-    owner: "Mona Lisa",
-  },
-  {
-    name: "Black Panther CARD",
-    number: "1234 5678 9101 XXXX",
-    owner: "Mona Lisa",
-  },
-  {
-    name: "Thor CARD",
-    number: "1234 5678 9101 XXXX",
-    owner: "Mona Lisa",
+    card_type: "Reward Card"
+
   },
 ];
 
@@ -46,9 +57,18 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 export default function MaximiseRewards() {
   const [tabValue, setTabValue] = React.useState(2); 
   const [openDialog, setOpenDialog] = React.useState(false);
+  const [current_cc, setCC] = React.useState("")
 
-  const handleOpenDialog = () => setOpenDialog(true);
-  const handleCloseDialog = () => setOpenDialog(false);
+  const handleOpenDialog = (cc_name) => () => {
+    console.log(cc_name)
+    setCC(cc_name)
+    setOpenDialog(true);
+  }
+  const handleCloseDialog = () => {
+    setCC("")
+    setOpenDialog(false);
+
+  }
 
   const navigate = useNavigate();
   const handleShowRewards = () => {
@@ -58,75 +78,87 @@ export default function MaximiseRewards() {
   const handleTermsAndCondition = () => {
     navigate('/t&c');
     };
+  console.log(current_cc)
 
 const handleRecommendationPage = () => {
         navigate('/recommendations');
         };
 
   return (
-    <Box sx={{ p: 2, maxWidth: 500, mx: 'auto' }}>
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Typography variant="h6" fontWeight="bold">Cards</Typography>
-        <Box display="flex" gap={1}>
-          <IconButton><NotificationsIcon /></IconButton>
-          <IconButton><SearchIcon /></IconButton>
-          <IconButton><MoreVertIcon /></IconButton>
+    <Box sx={{ maxWidth: 500, mx: 'auto', height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <div className='fixed header' style={{
+        position: 'sticky',
+        top: 0,
+        backgroundColor: 'white', // Add background color to avoid content showing through
+        zIndex: 10,
+        padding: '16px',
+        width: '100%',
+      }}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h6" fontWeight="bold">Cards</Typography>
+          <Box display="flex" gap={1}>
+            <IconButton><NotificationsIcon /></IconButton>
+            <IconButton><SearchIcon /></IconButton>
+            <IconButton><MoreVertIcon /></IconButton>
+          </Box>
         </Box>
-      </Box>
-
-      <Tabs
-        value={tabValue}
-        onChange={(e, newValue) => setTabValue(newValue)}
-        variant="fullWidth"
-        sx={{ mt: 2 }}
-      >
-        <Tab label="Cards" />
-        <Tab label="Find a card" />
-        <Tab label="Maximise Rewards" sx={tabValue === 2 ? {fontWeight: 'bold' } : {}} />
-      </Tabs>
-
-      <Box variant="outlined" sx={{ p: 2, borderRadius: 2, mt: 2 }}>
-        <Typography fontWeight="bold" fontSize={25} variant="body2">
-          Don't miss out on the chance to take full advantage of your rewards!
-        </Typography>
-        <Button variant="outlined" size="small" sx={{borderRadius:"200px !important",
-                mt: 1, color: '#008500', borderColor: '#008500'}} onClick={handleRecommendationPage}>
-          💰 Click here to learn more!
-        </Button>
-      </Box>
-
-      <Typography variant="h6" fontWeight="bold" sx={{ mt: 3 }}>Your Credit Cards</Typography>
-
-      <Box display="flex" flexDirection="column" gap={2} mt={2}>
-        {creditCards.map((card, index) => (
-          <Card key={index} variant="outlined" sx={{ borderRadius: 2}}>
-            <CardContent>
-              <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Typography variant="subtitle1" fontWeight="medium">{card.name}</Typography>
-                <Button variant="caption" color="text.secondary" onClick={handleOpenDialog}>Show T&Cs</Button>
-              </Box>
-              <Typography variant="body2" sx={{ fontFamily: 'monospace', mt: 1 }}>{card.number}</Typography>
-              <Box display="flex" justifyContent="space-between" alignItems="center" mt={1}>
-                <Typography variant="body2" color="text.secondary">{card.owner}</Typography>
-                <Button size="small" sx={{ color: '#008500' }} onClick={handleShowRewards} >Show Rewards &gt;</Button>
-              </Box>
-            </CardContent>
-          </Card>
-        ))}
-      </Box>
-
-      <Dialog
-        open={openDialog}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleCloseDialog}
-        fullWidth
-        PaperProps={{ sx: { borderRadius: '16px 16px 0 0', position: 'absolute', bottom: 0 } }}
-      >
-        <Box sx={{ p: 2 }}>
-          <TermsAndCondition />
+        <Tabs
+          value={tabValue}
+          onChange={(e, newValue) => setTabValue(newValue)}
+          variant="fullWidth"
+          sx={{ mt: 2 }}
+        >
+          <Tab label="Cards" />
+          <Tab label="Find a card" />
+          <Tab label="Maximise Rewards" sx={tabValue === 2 ? {fontWeight: 'bold' } : {}} />
+        </Tabs>
+      </div>
+      <div className='scorllable-body' style={{
+        overflowY: 'auto',
+        flex: 1,
+        padding: '16px',
+      }}>
+        <Box variant="outlined" sx={{ p: 2, borderRadius: 2, mt: 2 }}>
+          <Typography fontWeight="bold" fontSize={25} variant="body2">
+            Don't miss out on the chance to take full advantage of your rewards!
+          </Typography>
+          <Button variant="outlined" size="small" sx={{borderRadius:"200px !important",
+            mt: 1, color: '#008500', borderColor: '#008500'}} onClick={handleRecommendationPage}>
+            💰 Click here to learn more!
+          </Button>
         </Box>
-      </Dialog>
+        <Typography variant="h6" fontWeight="bold" sx={{ mt: 3 }}>Your Credit Cards</Typography>
+        <Box display="flex" flexDirection="column" gap={2} mt={2}>
+          {creditCards.map((card, index) => (
+            <Card key={index} variant="outlined" sx={{ borderRadius: 2}}>
+              <CardContent>
+                <Box display="flex" justifyContent="space-between" alignItems="center">
+                  <Typography variant="subtitle1" fontWeight="medium">{card.name}</Typography>
+                  <Button variant="caption" color="text.secondary" onClick={handleOpenDialog(card.name)}>Show T&Cs</Button>
+                </Box>
+                <Typography variant="body2" sx={{ fontFamily: 'monospace', mt: 1 }}>{card.number}</Typography>
+                <Box display="flex" justifyContent="space-between" alignItems="center" mt={1}>
+                  <Typography variant="body2" color="text.secondary">{card.owner}</Typography>
+                  <Button size="small" sx={{ color: '#008500' }} onClick={handleShowRewards} >Show Rewards &gt;</Button>
+                </Box>
+              </CardContent>
+            </Card>
+          ))}
+        </Box>
+        <Dialog
+          open={openDialog}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleCloseDialog}
+          fullWidth
+          PaperProps={{ sx: { borderRadius: '16px 16px 0 0', position: 'absolute', bottom: 0, maxWidth:500 } }}
+
+        >
+          <Box sx={{ p: 0 }}>
+            <TermsAndCondition cc_name={current_cc}/>
+          </Box>
+        </Dialog>
+      </div>
     </Box>
   );
 }
